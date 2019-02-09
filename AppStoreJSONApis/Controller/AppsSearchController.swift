eb3e286fb5406ef6c0cc545448a39fe410b3543c
Brian Voong <bhlvoong@gmail.com>
2019-02-08 22:06:59 -0800

    Ep 3 Smart Refactoring Patterns

---
+++ AppStoreJSONApis/Controller/AppsSearchController.swift
@@ -0,0 +1,27 @@
+//
+//  AppsSearchController.swift
+//  AppStoreJSONApis
+//
+//  Created by Brian Voong on 2/8/19.
+//  Copyright © 2019 Brian Voong. All rights reserved.
+//
+
+import UIKit
+
+class AppsSearchController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .red
+    }
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
