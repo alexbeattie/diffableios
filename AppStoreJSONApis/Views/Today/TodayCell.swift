a205a3d8c4a4bfe8d8f5b102e22cc1ba0674fb57
Brian Voong <bhlvoong@gmail.com>
2019-02-27 16:09:41 -0800

    Ep 28 Today Controller Cells

---
+++ AppStoreJSONApis/Views/Today/TodayCell.swift
@@ -0,0 +1,30 @@
+//
+//  TodayCell.swift
+//  AppStoreJSONApis
+//
+//  Created by Brian Voong on 2/27/19.
+//  Copyright © 2019 Brian Voong. All rights reserved.
+//
+
+import UIKit
+
+class TodayCell: UICollectionViewCell {
+    
+    let imageView = UIImageView(image: #imageLiteral(resourceName: "garden"))
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        layer.cornerRadius = 16
+        
+        addSubview(imageView)
+        imageView.contentMode = .scaleAspectFill
+        imageView.centerInSuperview(size: .init(width: 250, height: 250))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+}
