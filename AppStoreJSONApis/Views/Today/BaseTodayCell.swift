0a637a0d351ae7d80abfe79ae2e60d1d149e2bbe
Brian Voong <bhlvoong@gmail.com>
2019-03-02 15:51:09 -0800

    Ep 34 Multiple Today Cell Enumeration

---
+++ AppStoreJSONApis/Views/Today/BaseTodayCell.swift
@@ -0,0 +1,15 @@
+//
+//  BaseTodayCell.swift
+//  AppStoreJSONApis
+//
+//  Created by Brian Voong on 3/2/19.
+//  Copyright © 2019 Brian Voong. All rights reserved.
+//
+
+import UIKit
+
+class BaseTodayCell: UICollectionViewCell {
+    
+    var todayItem: TodayItem!
+    
+}
